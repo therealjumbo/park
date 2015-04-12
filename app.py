a2b13e76305623e64e58a45931e35ef700130ffa
Jeff Zignego <jlzignego@gmail.com>
2015-04-11 21:37:04 -0500

    app modification for change in url

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello World"
+parking_spots = [
+    {
+        'occupied': True
+    }
+]
+
+
+@app.route('/api/v1.0/spots/<int:spot_id>', methods=['GET'])
+def get_spots(spot_id):
+    return jsonify(parking_spots[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
