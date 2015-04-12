aae46d22839bce72e8d44bc17eff0c0cdae50bbc
Jeff Zignego <jlzignego@gmail.com>
2015-04-12 13:05:22 -0500

    views now reads serial val from db

--- app/views.py
+++ app/views.py
@@ -1,14 +1,11 @@
 from flask import jsonify
 from app import app
-
-
-parking_spots = [
-    {
-        'occupied': True
-    }
-]
+from pyserial import read_from_db
 
 
 @app.route('/api/v1.0/spots/<int:spot_id>', methods=['GET'])
 def get_spots(spot_id):
-    return jsonify(parking_spots[0])
+    val = read_from_db()
+    return jsonify({
+        'occupied': val
+    })
